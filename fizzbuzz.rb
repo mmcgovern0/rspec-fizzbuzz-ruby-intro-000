8c57cd25f81d1465aa6395a1ff3895e39da24be5
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-12 17:11:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(i)
-  if i % 3 == 0 
-    "Fizz" 
+  if i % 3 && i % 5 == 0 
+    "Fizzbuzz"
+  elsif i % 3
+    "Fizz"
+  elsif i % 5
+    "Buzz"
   end
 end
 
